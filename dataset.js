20927c83e17b6baca365059769c1eb72c59f4e67
lucasb-eyer <lucasb.eyer.be@gmail.com>
2015-07-21 12:18:17 +0200

    Initial project, not super-clean yet.

---
+++ dataset.js
@@ -0,0 +1,6 @@
+var dataset = [
+    {"file": "1392324011855.jpg", "label": "Foo"},
+    {"file": "1392332267190.jpg", "label": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
+    {"file": "1392324686782.jpg", "label": "Bar"},
+    {"file": "1392326040407.jpg", "label": "Baz"},
+]
